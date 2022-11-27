a033909e94688108b868f3adc9e0b2bc54b37bd6
SRcpr <completefrontend2021@gmail.com>
2022-11-27 07:06:56 +0530

    complete homepage layout

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,7 +1,68 @@
+import Rightbar from "../rightbar/Rightbar";
+import Sidebar from "../sidebar/Sidebar";
 import "./feed.scss";
 
 const Feed = () => {
-  return <div className="feed">Feed</div>;
+  return (
+    <div className="feed">
+      <Sidebar />
+      <div className="feedContainers">
+        <div className="feedContainer">
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+        </div>
+      </div>
+      <Rightbar />
+    </div>
+  );
 };
 
 export default Feed;
--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,6 +1,4 @@
 import Feed from "../../components/feed/Feed";
-import Rightbar from "../../components/rightbar/Rightbar";
-import Sidebar from "../../components/sidebar/Sidebar";
 import Topbar from "../../components/topbar/Topbar";
 import "./home.scss";
 
@@ -9,9 +7,7 @@ const Home = () => {
     <>
       <Topbar />
       <div className="home">
-        <Sidebar />
         <Feed />
-        <Rightbar />
       </div>
     </>
   );
